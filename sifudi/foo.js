34a4dc55c6195aac0b3137e54b0b2db3e5d7e5c4
Roger Zhang <1787009521@qq.com>
2022-11-02 16:49:13 +0800

    feat: 思迪福

---
+++ sifudi/foo.js
@@ -0,0 +1,9 @@
+var arr1 = "ab".split('')
+var arr2 = arr1.reverse()
+var arr3 = "abc".split('')
+arr2.push(arr3)
+
+console.log(arr1.length)
+console.log(arr1.slice(-1))
+console.log(arr2.length)
+console.log(arr1.slice(-1))
